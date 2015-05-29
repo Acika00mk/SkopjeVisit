f91039c1c35b58d49ffa03f4a8609947fe1444ba
acika00mk <aleksandar@kostadinovski.info>
2015-05-30 00:14:26 +0200

    init2

--- js/app.js
+++ js/app.js
@@ -47,11 +47,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
-    .state('app.playlists', {
-      url: "/playlists",
+    .state('app.start', {
+      url: "/start",
       views: {
         'menuContent': {
-          templateUrl: "templates/playlists.html",
+          templateUrl: "templates/startScreen.html",
           controller: 'PlaylistsCtrl'
         }
       }
@@ -67,5 +67,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/start');
 });
